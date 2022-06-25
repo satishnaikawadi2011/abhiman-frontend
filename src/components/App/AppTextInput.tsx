075feed6e025909d72e3c7f3692b96f3dc9d8815
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2022-06-26 02:46:42 +0530

    ✨ feat : add AppTextInput component

---
+++ src/components/App/AppTextInput.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { TextInputProps, ViewStyle, StyleSheet, View, TextInput } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { Colors } from '../../constants/colors';
+// import { useTheme } from 'react-native-paper';
+
+export interface InputProps {
+	style?: ViewStyle;
+	icon?: any;
+}
+
+const AppTextInput: React.FC<TextInputProps & InputProps> = ({ style, icon, ...props }) => {
+	// const theme = useTheme();
+	return (
+		<View
+			style={[
+				styles.container,
+				{
+					// backgroundColor:
+					// 		theme.dark ? Colors.darkTextInputBg :
+					// 		Colors.lightTextInputBg
+				},
+				style
+			]}
+		>
+			{icon && (
+				<MaterialCommunityIcons
+					style={styles.icon}
+					// color={theme.colors.placeholder}
+					name={icon}
+					size={20}
+				/>
+			)}
+			<TextInput
+				// placeholderTextColor={theme.colors.placeholder}
+				style={[
+					styles.textInput
+					// { color: theme.colors.text }
+				]}
+				{...props}
+			/>
+		</View>
+	);
+};
+
+export default AppTextInput;
+
+const styles = StyleSheet.create({
+	container:
+		{
+			borderRadius: 25,
+			flexDirection: 'row',
+			width: '95%',
+			padding: 15,
+			alignSelf: 'center',
+			marginVertical: 10,
+			alignItems: 'center',
+			overflow: 'hidden'
+		},
+	icon:
+		{
+			marginHorizontal: 10
+		},
+	textInput:
+		{
+			fontFamily: 'UbuntuRegular',
+			fontSize: 18
+		}
+});
---
+++ src/screens/AddStoryScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const AddStoryScreen = () => {
+	return (
+		<View>
+			<Text>AddStoryScreen</Text>
+		</View>
+	);
+};
+
+export default AddStoryScreen;
+
+const styles = StyleSheet.create({});
