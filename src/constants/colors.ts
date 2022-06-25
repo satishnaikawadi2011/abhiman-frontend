596ca69359127f3eba1aab47c96559509561adaf
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2022-06-25 13:03:50 +0530

    ✨ feat : add some constants

---
+++ src/constants/colors.ts
@@ -0,0 +1,11 @@
+export const Colors = {
+	primary: '#ff6699',
+	accent: '#3366cc',
+	dark: '#1c7c54',
+	black: '#000000',
+	white: '#ffffff',
+	medium: 'rgb(128, 128, 128)',
+	light: ' #f2f2f2',
+	lightTextInputBg: '#e6e6e6',
+	darkTextInputBg: 'rgb(89, 89, 89)'
+};
---
+++ src/constants/index.ts
@@ -0,0 +1,3 @@
+import { Dimensions } from 'react-native';
+export const DEVICE_WIDTH = Dimensions.get('window').width;
+export const DEVICE_HEIGHT = Dimensions.get('window').height;
